b5909c6f455bba45ddfaeecf7f37a7fe1256e4c1
WendaLi <lwd19861127@hotmail.com>
2020-04-03 10:40:18 +0800

    Initial Commit

---
+++ Threelow/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Threelow
+//
+//  Created by WendaLi on 2020-04-03.
+//  Copyright © 2020 WendaLi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
